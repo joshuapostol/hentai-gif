ef0fa38e6a43a958240c664b39ba9989b096945f
joshuapostol <156434541+joshuapostol@users.noreply.github.com>
2024-04-28 11:47:42 +0000

    Hello

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const fs = require('fs');
+const axios = require('axios');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Welcome message
 app.get('/', (req, res) => {
   res.send('Welcome to Joshua Apostol API');
 });
 
-// Random GIF endpoint
-app.get('/random-gif', (req, res) => {
+app.get('/random-gif', async (req, res) => {
   try {
-    fs.readFile('random_gif.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      const jsonData = JSON.parse(data);
-      const randomIndex = Math.floor(Math.random() * jsonData.gifLinks.length);
-      const randomGif = jsonData.gifLinks[randomIndex];
-      res.send(`<img src="${randomGif}" alt="Random GIF">`);
-    });
+    const response = await axios.get('http://localhost:3000/random-gif-links');
+    const gifLinks = response.data.gifLinks;
+    const randomIndex = Math.floor(Math.random() * gifLinks.length);
+    const randomGif = gifLinks[randomIndex];
+    res.json({ gifLink: randomGif });
   } catch (error) {
     console.error('Error fetching random GIF:', error);
     res.status(500).json({ error: 'Error fetching random GIF' });
   }
 });
 
-// Start server
+app.get('/random-gif-links', (req, res) => {
+  try {
+    fs.readFile('random_gif.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    });
+  } catch (error) {
+    console.error('Error fetching random GIF links:', error);
+    res.status(500).json({ error: 'Error fetching random GIF links' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
